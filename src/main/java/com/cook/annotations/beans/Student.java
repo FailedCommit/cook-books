5c89d0c4964a533ba974897cc91fb0437296c22f
Mohit Kumar <mkumar@tekion.com>
2020-09-14 19:44:17 +0530

    Adds custom annotations

---
+++ src/main/java/com/cook/annotations/beans/Student.java
@@ -0,0 +1,32 @@
+package com.cook.annotations.beans;
+
+import com.cook.annotations.custom.Init;
+import com.cook.annotations.custom.JsonElement;
+import com.cook.annotations.custom.JsonSerializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@JsonSerializable
+public class Student {
+    @JsonElement
+    private String firstName;
+
+    @JsonElement
+    private String lastName;
+
+    @JsonElement(key = "studentAge")
+    private double age;
+
+    /**
+     * Ensures that firstName and lastName starts with Uppercase and rest is all Lowercase.
+     */
+    @Init
+    private void formatNames() {
+        this.firstName = this.firstName.substring(0, 1).toUpperCase() +
+                this.firstName.substring(1).toLowerCase();
+        this.lastName = this.lastName.substring(0, 1).toUpperCase() +
+                this.lastName.substring(1).toUpperCase();
+    }
+}
---
+++ src/main/java/com/cook/annotations/processors/Serializer.java
@@ -0,0 +1,71 @@
+package com.cook.annotations.processors;
+
+import com.cook.annotations.custom.Init;
+import com.cook.annotations.custom.JsonElement;
+import com.cook.annotations.custom.JsonSerializable;
+import com.cook.annotations.exceptions.SerializationException;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static java.util.Objects.isNull;
+
+public class Serializer {
+    public String serialize(Object obj) throws SerializationException {
+        try {
+            checkIfSerializable(obj);
+            initializeObject(obj);
+            return getJsonString(obj);
+        } catch(Exception e) {
+            throw new SerializationException(e.getMessage());
+        }
+    }
+
+
+    private void checkIfSerializable(Object obj) {
+        if(isNull(obj)) {
+            throw new SerializationException("Can't serialize null object");
+        }
+        Class<?> clazz = obj.getClass();
+        if(!clazz.isAnnotationPresent(JsonSerializable.class)) {
+            throw new SerializationException(
+                    "The class " + clazz.getSimpleName() + " is not annotated with JsonSerializable annotation");
+        }
+    }
+
+    private void initializeObject(Object obj) throws InvocationTargetException, IllegalAccessException {
+        Class<?> clazz = obj.getClass();
+        for(Method method : clazz.getDeclaredMethods()) {
+            if(method.isAnnotationPresent(Init.class)) {
+                method.setAccessible(true);
+                method.invoke(obj);
+            }
+        }
+    }
+
+    private String getJsonString(Object obj) throws IllegalAccessException {
+        Class<?> clazz = obj.getClass();
+        Map<String, String> elemMap = new HashMap<>();
+        for(Field field : clazz.getDeclaredFields()) {
+            field.setAccessible(true);
+            if(field.isAnnotationPresent(JsonElement.class)) {
+                elemMap.put(getKey(field), (String) field.get(obj));
+            }
+        }
+        String jsonString = elemMap.entrySet()
+                .stream()
+                .map(entry -> "\"" + entry.getKey() + "\":\"" + entry.getValue() + "\"")
+                .collect(Collectors.joining(","));
+        return "{" + jsonString + "}";
+    }
+
+    private String getKey(Field field) {
+        String value = field.getAnnotation(JsonElement.class).key();
+        return value.isEmpty() ? field.getName() : value;
+    }
+}
